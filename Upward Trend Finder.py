07541ee87ad9e1e20e65fb4474b29eb340e6f446
Batuhan Oran <117276804+oranbatu@users.noreply.github.com>
2023-09-25 14:27:29 +0200

    Upward Trend Finder.py

--- Upward Trend Finder.py
+++ Upward Trend Finder.py
@@ -1,127 +1,132 @@
-# Import necessary libraries
 import ccxt.async_support as ccxt_async
 import pandas as pd
-import numpy as np
 import asyncio
-import matplotlib.pyplot as plt
-import datetime
 import mplfinance as mpf
 
-# Initialize the Binance exchange object
+# Create an asynchronous Binance exchange instance with rate limiting enabled
 exchange = ccxt_async.binance({'enableRateLimit': True})
 
-# Define the trading pair symbol, timeframe, data limit, and consecutive count for lows and highs
-symbol = 'ETH/USDT'
-timeframe = "1d"
-limit = 400
-windows = 3
+# Example usage:
+symbol = 'ETH/USDT'  # The trading pair symbol
+timeframe = "4h"  # The timeframe for OHLCV data
+limit = 1000  # The maximum number of data points to retrieve
+windows = 3  # Number of consecutive windows for pattern detection
 
-# Function to fetch OHLCV data from the exchange
+# Asynchronous function to fetch OHLCV (Open, High, Low, Close, Volume) data
 async def fetch_ohlcv(symbol, timeframe, limit):
-    ohlcv = await exchange.fetch_ohlcv(symbol=symbol, timeframe=timeframe, limit=limit)
-    df = pd.DataFrame(ohlcv, columns=['timestamp', 'open', 'high', 'low', 'close', 'volume'])
-    return df
+    try:
+        ohlcv = await exchange.fetch_ohlcv(symbol=symbol, timeframe=timeframe, limit=limit)
+        df = pd.DataFrame(ohlcv, columns=['timestamp', 'open', 'high', 'low', 'close', 'volume'])
+        return df
+    except Exception as e:
+        print(f"Error fetching OHLCV data: {str(e)}")
+        return None
 
-# Function to find significant lows in the OHLCV data
-async def find_significant_lows(symbol, timeframe, limit):
-    df = await fetch_ohlcv(symbol, timeframe, limit)
-    lows = []  # Initialize an empty list to store lows
-    consecutive_lows = 0  # Initialize a counter for consecutive lows
-    all_lows = []  # Initialize a list to store all sets of significant lows
-
-    for i in range(len(df)):
-        if len(lows) == 0:
-            lows.append(df['low'][i])  # Add the low of the first candle to the list
-        else:
-            previous_low = lows[-1]  
-            current_low = df['low'][i] 
-
-            if previous_low <= current_low:
-                lows.append(current_low)
-                consecutive_lows = 0  
-            else:
-                consecutive_lows += 1
-                if consecutive_lows >= 4:  # Check for a sequence of 5 consecutive lows
-                    consecutive_lows = 0
-                    lows = []
-
-        if len(lows) == 6:  
-            all_lows.append(lows)
-            lows = []
-
-    # Print the shape and content of significant lows
-    print("all_lows shape:", len(all_lows), "x", len(all_lows[0]) if all_lows else 0)
-    print("all_lows content:")
-    for i, sublist in enumerate(all_lows):
-        print(f"Sublist {i + 1}: {sublist}")
-    return all_lows
-
-# Function to find significant highs in the OHLCV data
-async def find_significant_highs(symbol, timeframe, limit):
-    df = await fetch_ohlcv(symbol, timeframe, limit)
-    highs = [] # Initialize an empty list to store highs
-    consecutive_highs = 0  # Initialize a counter for consecutive highs
-    all_highs = []  # Initialize a list to store all sets of significant highs
-
-    for i in range(len(df)):
-        if len(highs) == 0:
-            highs.append(df['high'][i])  
-        else:
-            previous_high = highs[-1]  # Get the previous high
-            current_high = df['high'][i]  # Get the current high
-
-            if previous_high <= current_high:
-                highs.append(current_high)
-                consecutive_highs = 0  
-            else:
-                consecutive_highs += 1
-                if consecutive_highs >= 4:  # Check for a sequence of 5 consecutive highs
-                    consecutive_highs = 0
-                    highs = []
-
-        if len(highs) == 6: 
-            all_highs.append(highs)
-            highs = []
-
-    # Print the shape and content of significant highs
-    print("all_highs shape:", len(all_highs), "x", len(all_highs[0]) if all_highs else 0)
-    print("all_highs content:")
-    for i, sublist in enumerate(all_highs):
-        print(f"Sublist {i + 1}: {sublist}")
-    return all_highs
-
-# Function to plot candlestick charts for significant lows and highs
-async def plot_significant_lows_highs(symbol, timeframe, limit, min_low_count, min_high_count):
-    all_lows = await find_significant_lows(symbol, timeframe, limit)
-    all_highs = await find_significant_highs(symbol, timeframe, limit)
-
-    # Check if there are enough significant lows and highs to plot
-    if all(len(sublist) >= min_low_count for sublist in all_lows) and all(len(sublist) >= min_high_count for sublist in all_highs):
+# Asynchronous function to find significant lows and highs in the OHLCV data
+async def find_significant_lows_highs(symbol, timeframe, limit):
+    try:
         df = await fetch_ohlcv(symbol, timeframe, limit)
+        if df is None:
+            return None
+
+        lows = []  # Create an empty list to store significant lows
+        highs = []  # Create an empty list to store significant highs
+        consecutive_lows = 0
+        consecutive_highs = 0
+        all_lows = []  # Store lists of consecutive lows
+
+        for i in range(len(df)):
+            if len(lows) == 0:
+                lows.append((df['low'][i], i))  # Append (value, index) pair to lows list
+                highs.append((df["high"][i], i))  # Append (value, index) pair to highs list
+            else:
+                previous_low, previous_low_index = lows[-1]
+                current_low, current_low_index = df['low'][i], i
+                previous_high, previous_high_index = highs[-1]
+                current_high, current_high_index = df["high"][i], i
+
+                if previous_low < current_low:
+                    consecutive_lows = 0
+                    if previous_high < current_high:
+                        lows.append((current_low, current_low_index))
+                        highs.append((current_high, current_high_index))
+                        consecutive_highs = 0
+                    else:
+                        consecutive_highs += 1
+                        if consecutive_highs >= 1:
+                            lows = []
+                            highs = []
+                            consecutive_highs = 0
+                            consecutive_lows = 0
+                        else:
+                            lows.append((current_low, current_low_index))
+                            highs.append((current_high, current_high_index))
+                else:
+                    consecutive_lows += 1
+                    if consecutive_lows >= 2:
+                        consecutive_highs = 0
+                        consecutive_lows = 0
+                        lows = []
+                        highs = []
+
+            if len(lows) == 5:
+                all_lows.append(lows)
+                lows = []
+
+        print("Shape of all_lows:", len(all_lows), "x", len(all_lows[0]) if all_lows else 0)
+        print("Contents of all_lows:")
+        for i, sublist in enumerate(all_lows):
+            print(f"Sublist {i + 1}: {sublist}")
+
+        return all_lows
+    except Exception as e:
+        print(f"Error finding significant lows and highs: {str(e)}")
+        return None
+
+# Asynchronous function to plot significant lows and highs using mplfinance
+async def plot_significant_lows_highs(symbol, timeframe, limit, min_low_count, min_high_count):
+    try:
+        all_lows = await find_significant_lows_highs(symbol, timeframe, limit)
+
+        if all_lows is None:
+            return
+
+        df = await fetch_ohlcv(symbol, timeframe, limit)
+
+        if df is None:
+            return
 
         # Convert timestamp to a more readable date format
         df['timestamp'] = pd.to_datetime(df['timestamp'], unit='ms')
 
         for sublist in all_lows:
-            first_low_index = df[df['low'] == sublist[0]].index[0]
-            last_low_index = df[df['low'] == sublist[-1]].index[0]
-            df_subset = df[first_low_index:last_low_index + 1]
+            low_indices = [low_index for _, low_index in sublist]
+            first_low_index = min(low_indices)
+            last_low_index = max(low_indices)
+
+            df_subset = df.iloc[first_low_index:last_low_index + 1]
+
             ohlc_data = df_subset[['timestamp', 'open', 'high', 'low', 'close']]
             ohlc_data.set_index('timestamp', inplace=True)
-            mpf.plot(ohlc_data, type='candle', style='yahoo', title=f'Upward Trends for {symbol} ({timeframe})')
-    else:
-        print(f"Not enough significant lows found. Expected at least {min_low_count} or more elements in each sublist.")
 
-# Main asynchronous function
+            date_format = '%Y-%m-%d %H:%M:%S'
+
+            mpf.plot(ohlc_data, type='candle', style='yahoo', title=f'Upward Trends for {symbol} ({timeframe})',
+                     datetime_format=date_format)
+    except Exception as e:
+        print(f"Error plotting significant lows and highs: {str(e)}")
+
+# Asynchronous main function
 async def main():
     try:
         await plot_significant_lows_highs(symbol, timeframe, limit, min_low_count=6, min_high_count=6)
+
     except Exception as e:
         print(f"An error occurred: {str(e)}")
     finally:
         # Close the exchange instance and related resources
         await exchange.close()
 
-# Execute the main function
 if __name__ == "__main__":
     asyncio.run(main())
+
